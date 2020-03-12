f5bffc2bb071e0cbbd37aa13e15f078781bca601
Tian Qin <qintian29@gmail.com>
2020-03-12 18:31:21 +0800

    another test

---
+++ client/build/precache-manifest.2471dc703056de156022340ad0de0739.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e44e8cbbd58ba0b781e2212105b1ba50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0191675d516a0b1f8a6",
+    "url": "/static/css/main.5e80492f.chunk.css"
+  },
+  {
+    "revision": "df2c471922fc7389e555",
+    "url": "/static/js/2.96bee6cb.chunk.js"
+  },
+  {
+    "revision": "d4773cfc8952e56a2df4dd3d885d1f23",
+    "url": "/static/js/2.96bee6cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0191675d516a0b1f8a6",
+    "url": "/static/js/main.d1892845.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  },
+  {
+    "revision": "3ca838c7b09cc4b940ac95caaa54e89d",
+    "url": "/static/media/backgroundhome.3ca838c7.jpg"
+  },
+  {
+    "revision": "294158a1a95d74df281d4f0254242a37",
+    "url": "/static/media/color.294158a1.jpg"
+  },
+  {
+    "revision": "4118acdc790ce09f17312fec0dd2335c",
+    "url": "/static/media/profilebg.4118acdc.jpg"
+  }
+]);
